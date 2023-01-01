42a7d00dc5c5b71019acb0a4bf88806c38e19651
oenea <oenear@gmail.com>
2023-01-01 18:34:59 +0100

    a

--- src/views/includes/head.inc.php
+++ src/views/includes/head.inc.php
@@ -1,8 +1,8 @@
     <meta charset="UTF-8">
     <meta name="description" content="Wai2">
     <meta name="keywords" content="Wai, mongodb, php, html, css">
-    <link rel="stylesheet" href="web/static/style/style.css" type="text/css">
+    <!--<link rel="stylesheet" href="web/static/style/style.css" type="text/css">-->
     <title>WAI2</title>
-    <script src="web/static/scripts/main.js" type="text/javascript"></script>
+    <!--<script src="web/static/scripts/main.js" type="text/javascript"></script>-->
 
 
